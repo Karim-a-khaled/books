431cdc59c86688e06993d66238adf136dfb4337c
Karim Abdallah <karim.a.s.khaled@gmail.com>
2023-06-13 18:13:49 +0000

    rendered the books and currently working on filtering the books

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -8,56 +8,49 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 export default function Store() {
-  // const [searchedBooks,setBook] = useState(books)
+  const [searchedBooks,setBook] = useState(books)
 
-  return( 
+  function filteredBooks(e){
+    const theBook = books.filter((book)=>{
+      return book.title.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
+    })
+    console.log(theBook)
+    setBook(theBook)
+  }
+
+  function displayBooks() {
+    return books.map((book) => (
+      <>
+      <Card className='main-card-custom' key={book.id} >
+        <Card.Img variant="top" src={book.image} />
+        <Card.Body className='card-body-custom'>
+          <Card.Title>{book.title}</Card.Title>
+          <Card.Text className='card-text-custom'>{book.description}</Card.Text>
+          <div className="btn-span d-flex">
+            <Link to={`/${book.id}`} ><Button className="btn-custom">View Book</Button></Link>
+            <span className="price">$150.00</span>
+          </div>
+        </Card.Body>
+      </Card>
+      </>
+    ));
+  }
+
+return( 
   <>
-  <div className="container input-container-custom">
-    <input type="text" onChange={filteredBooks} placeholder='Search A Book..' className='input-custom' />
-  </div>
-  <div className="container container-custom">{{displayBooks}}</div>
+    <div className="container input-container-custom"> 
+      <input type="text" onChange={filteredBooks}  placeholder='Search A Book..' className='input-custom' />
+    </div>
+    <div className="container container-custom">{displayBooks()}</div>
   </>
-  )
+)
 
-function displayBooks() {
-  return books.map((book) => (
-    <Card className='main-card-custom' style={{ width: '22rem' }} key={book.title}>
-      <Card.Img variant="top" src={book.image} />
-      <Card.Body className='card-body-custom'>
-        <Card.Title>{book.title}</Card.Title>
-        <Card.Text className='card-text-custom'>{book.description}</Card.Text>
-        <div className="btn-span d-flex">
-          <Link to={`/${book.id}`} ><Button className="btn-custom align-self-end">View Book</Button></Link>
-          <span className="price">$150.00</span>
-        </div>
-      </Card.Body>
-    </Card>
-  ));
+
+
+
+
+
+
+
 }
 
-function filteredBooks(e){
-  const theBooks = books.filter((book)=>{
-    return book.title.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
-  })
-  // setBook(theBooks)
-}
-}
-
-
-
- 
-{/* {
- searchedBooks.map((book) =>(
-    <Card className='main-card-custom' style={{ width: '22rem' }} key={book.title}>
-      <Card.Img variant="top" src={book.image} />
-      <Card.Body className='card-body-custom'>
-        <Card.Title>{book.title}</Card.Title>
-        <Card.Text className='card-text-custom'>{book.description}</Card.Text>
-        <div className="btn-span d-flex">
-          <Link to={`/${book.id}`} ><Button className="btn-custom align-self-end">View Book</Button></Link>
-          <span className="price">$150.00</span>
-        </div>
-      </Card.Body>
-    </Card>
-  ))} */}
-  {/* </div> */}
